9d86144aafad9d055494db206ac7ee8f2abcb5a5
Edi Wang <Edi.Wang@outlook.com>
2020-02-09 17:26:15 +0800

    Create JCBBINValidator.cs

---
+++ src/Edi.CreditCardUtils/BINValidators/JCBBINValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edi.CreditCardUtils.BINValidators
+{
+    public class JCBBINValidator : IBINFormatValidator
+    {
+        public string BrandName => "JCB";
+        public string BrandRegEx => @"^(?:2131|1800|35\d{3})\d{11}$";
+    }
+}
