9e88b0ce83fd3ddff8bd078ed7b0e8839f7a91c9
Edi Wang <Edi.Wang@outlook.com>
2020-02-09 17:27:42 +0800

    Create DiscoverBINValidator.cs

---
+++ src/Edi.CreditCardUtils/BINValidators/DiscoverBINValidator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edi.CreditCardUtils.BINValidators
+{
+    public class DiscoverBINValidator : IBINFormatValidator
+    {
+        public string BrandName => "Discover";
+
+        public string BrandRegEx => "^65[4-9][0-9]{13}|64[4-9][0-9]{13}|6011[0-9]{12}|(622(?:12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[01][0-9]|92[0-5])[0-9]{10})$";
+    }
+}
