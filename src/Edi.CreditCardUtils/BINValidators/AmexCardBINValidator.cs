f8bcd0b5c2da620cd8f9e633b1851f5813043480
Edi Wang <Edi.Wang@outlook.com>
2020-02-09 11:33:27 +0800

    Create AmexCardBINValidator.cs

---
+++ src/Edi.CreditCardUtils/BINValidators/AmexCardBINValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edi.CreditCardUtils.BINValidators
+{
+    public class AmexCardBINValidator : IBINFormatValidator
+    {
+        public string BrandName => "Amex Card";
+        public string BrandRegEx => "^3[47][0-9]{13}$";
+    }
+}
