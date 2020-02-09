68bb785786cd6546d5f2be030c9f5e4190e439b2
Edi Wang <Edi.Wang@outlook.com>
2020-02-09 11:34:43 +0800

    Create UnionPayCardValidator.cs

---
+++ src/Edi.CreditCardUtils/BINValidators/UnionPayCardValidator.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Edi.CreditCardUtils.BINValidators
+{
+    public class UnionPayCardValidator : IBINFormatValidator
+    {
+        public string BrandName => "Union Pay";
+        public string BrandRegEx => "^(62[0-9]{14,17})$";
+    }
+}
