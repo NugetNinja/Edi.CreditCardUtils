9a9652411ff35bbf4aed4d02fe8ea4851d663a56
Edi Wang <Edi.Wang@outlook.com>
2021-11-04 14:41:49 +0800

    upgrade to .NET 6.0

--- src/Edi.CreditCardUtils.Tests/CreditCardGeneratorTests.cs
+++ src/Edi.CreditCardUtils.Tests/CreditCardGeneratorTests.cs
@@ -1,45 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
+﻿using NUnit.Framework;
 
-namespace Edi.CreditCardUtils.Tests
+namespace Edi.CreditCardUtils.Tests;
+
+[TestFixture]
+public class CreditCardGeneratorTests
 {
-    [TestFixture]
-    public class CreditCardGeneratorTests
+    [Test]
+    public void TestGenerateCardNumber()
     {
-        [Test]
-        public void TestGenerateCardNumber()
+        var bin = "485246";
+        int length = 16;
+        var cn = CreditCardGenerator.GenerateCardNumber(bin, length);
+
+        Assert.IsNotEmpty(cn);
+        Assert.IsTrue(cn.Length == length);
+
+        var result = CreditCardValidator.ValidCardNumber(cn);
+
+        Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_LuhnOnly
+                      || result.CardNumberFormat == CardNumberFormat.Valid_BINTest);
+    }
+
+    [Test]
+    public void TestGenerateBatchCardNumbers()
+    {
+        var bin = "485246";
+        int length = 16;
+
+        var cardNumbers = new List<string>();
+        for (int i = 0; i < 128; i++)
         {
-            var bin = "485246";
-            int length = 16;
             var cn = CreditCardGenerator.GenerateCardNumber(bin, length);
-
-            Assert.IsNotEmpty(cn);
-            Assert.IsTrue(cn.Length == length);
-
-            var result = CreditCardValidator.ValidCardNumber(cn);
-
-            Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_LuhnOnly
-                          || result.CardNumberFormat == CardNumberFormat.Valid_BINTest);
+            cardNumbers.Add(cn);
         }
 
-        [Test]
-        public void TestGenerateBatchCardNumbers()
-        {
-            var bin = "485246";
-            int length = 16;
-
-            var cardNumbers = new List<string>();
-            for (int i = 0; i < 128; i++)
-            {
-                var cn = CreditCardGenerator.GenerateCardNumber(bin, length);
-                cardNumbers.Add(cn);
-            }
-
-            var isUnique = cardNumbers.GroupBy(x => x).All(g => g.Count() == 1);
-            Assert.IsTrue(isUnique);
-        }
+        var isUnique = cardNumbers.GroupBy(x => x).All(g => g.Count() == 1);
+        Assert.IsTrue(isUnique);
     }
 }
--- src/Edi.CreditCardUtils.Tests/CreditCardValidatorTests.cs
+++ src/Edi.CreditCardUtils.Tests/CreditCardValidatorTests.cs
@@ -1,77 +1,74 @@
-using System.Collections.Generic;
-using System.Linq;
 using Edi.CreditCardUtils.BINValidators;
 using NUnit.Framework;
 
-namespace Edi.CreditCardUtils.Tests
+namespace Edi.CreditCardUtils.Tests;
+
+[TestFixture]
+public class Tests
 {
-    [TestFixture]
-    public class Tests
+    public static IEnumerable<TestCaseData> InvalidCardNumbersData
     {
-        public static IEnumerable<TestCaseData> InvalidCardNumbersData
+        get
         {
-            get
-            {
-                yield return new TestCaseData(null);
-                yield return new TestCaseData("");
-                yield return new TestCaseData(" ");
-                yield return new TestCaseData("123");
-                yield return new TestCaseData("abc");
-                yield return new TestCaseData("123abc");
-                yield return new TestCaseData("abc123");
-                yield return new TestCaseData("1111 2222");
-            }
+            yield return new TestCaseData(null);
+            yield return new TestCaseData("");
+            yield return new TestCaseData(" ");
+            yield return new TestCaseData("123");
+            yield return new TestCaseData("abc");
+            yield return new TestCaseData("123abc");
+            yield return new TestCaseData("abc123");
+            yield return new TestCaseData("1111 2222");
         }
+    }
 
-        [TestCaseSource("InvalidCardNumbersData")]
-        public void TestInvalidCardNumberFormat(string cardNumber)
-        {
-            var result = CreditCardValidator.ValidCardNumber(cardNumber);
-            Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Invalid_BadStringFormat);
-        }
+    [TestCaseSource("InvalidCardNumbersData")]
+    public void TestInvalidCardNumberFormat(string cardNumber)
+    {
+        var result = CreditCardValidator.ValidCardNumber(cardNumber);
+        Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Invalid_BadStringFormat);
+    }
 
-        [Test]
-        public void TestLuhnMod10Failure()
-        {
-            var result = CreditCardValidator.ValidCardNumber("9962514040073500");
-            Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Invalid_LuhnFailure);
-        }
+    [Test]
+    public void TestLuhnMod10Failure()
+    {
+        var result = CreditCardValidator.ValidCardNumber("9962514040073500");
+        Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Invalid_LuhnFailure);
+    }
 
-        [Test]
-        public void TestLuhnMod10Success()
-        {
-            var result = CreditCardValidator.ValidCardNumber("214916279426729");
-            Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_LuhnOnly);
-        }
+    [Test]
+    public void TestLuhnMod10Success()
+    {
+        var result = CreditCardValidator.ValidCardNumber("214916279426729");
+        Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_LuhnOnly);
+    }
 
-        [Test]
-        public void TestValidVisa()
-        {
-            var result = CreditCardValidator.ValidCardNumber("4012888888881881");
-            Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_BINTest && result.CardTypes.Contains("Visa"));
-        }
+    [Test]
+    public void TestValidVisa()
+    {
+        var result = CreditCardValidator.ValidCardNumber("4012888888881881");
+        Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_BINTest && result.CardTypes.Contains("Visa"));
+    }
 
-        [Test]
-        public void TestValidMasterCard()
-        {
-            var result = CreditCardValidator.ValidCardNumber("5105105105105100");
-            Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_BINTest && result.CardTypes.Contains("MasterCard"));
-        }
+    [Test]
+    public void TestValidMasterCard()
+    {
+        var result = CreditCardValidator.ValidCardNumber("5105105105105100");
+        Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_BINTest && result.CardTypes.Contains("MasterCard"));
+    }
 
-        public class WellsFargoBankValidator : ICardTypeValidator
-        {
-            public string Name => "Wells Fargo Bank";
-            public string RegEx => @"^(485246)\d{10}$";
-        }
+    public class WellsFargoBankValidator : ICardTypeValidator
+    {
+        public string Name => "Wells Fargo Bank";
+        public string RegEx => @"^(485246)\d{10}$";
+    }
 
-        [Test]
-        public void TestCardTypeValidator()
+    [Test]
+    public void TestCardTypeValidator()
+    {
+        var result = CreditCardValidator.ValidCardNumber("4852461030260066", new ICardTypeValidator[]
         {
-            var result = CreditCardValidator.ValidCardNumber("4852461030260066", new ICardTypeValidator[]
-            {
-                new WellsFargoBankValidator()
-            });
-            Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_BINTest && result.CardTypes.Contains("Wells Fargo Bank"));
-        }
+            new WellsFargoBankValidator()
+        });
+        Assert.IsTrue(result.CardNumberFormat == CardNumberFormat.Valid_BINTest && result.CardTypes.Contains("Wells Fargo Bank"));
     }
 }
